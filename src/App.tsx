c1c5316a818a6fbca735120035716ff428caed6f
ketikketik <aazhard8@gmail.com>
2023-07-31 00:19:00 +0700

    Add all feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
+import {
+  AnimeListPage,
+  AnimeDetailPage,
+  CollectionListPage,
+  CollectionDetailPage,
+} from "./pages";
+import { Body, Navbar } from "./components";
+import CollectionProvider from "./context";
+
+const App = () => {
+  console.log("Render App");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Body>
+        <Switch>
+          <CollectionProvider>
+            <Route path="/" exact component={AnimeListPage} />
+            <Route path="/anime/:id" component={AnimeDetailPage} />
+            <Route path="/collection" exact component={CollectionListPage} />
+            <Route path="/collection/:name" component={CollectionDetailPage} />
+          </CollectionProvider>
+        </Switch>
+      </Body>
+    </Router>
   );
-}
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://graphql.anilist.co",
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+    ,
   </React.StrictMode>
 );
 
